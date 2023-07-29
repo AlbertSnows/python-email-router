58c12b50221b35211a4e91a57be650666680f714
Albert Snow <ajsnow2012@gmail.com>
2023-07-28 21:19:43 -0500

    initial commit  bringing files over from flask tutorial I toyed around with

---
+++ dev/example/hello.py
@@ -0,0 +1,11 @@
+# works with -> python hello.py
+from  flask import Flask;
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return 'Hello, World!'
+
+if __name__ == "__main__":
+  app.run(debug=True)
