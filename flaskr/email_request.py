5a2e6a4ab74f2ac816389d5707388d652d7fc46c
Albert Snow <ajsnow2012@gmail.com>
2023-07-28 22:30:25 -0500

    email field first draft

--- flaskr/email_request.py
+++ flaskr/email_request.py
@@ -1,29 +1,21 @@
 from wtforms import (
     Form,
-    BooleanField,
     StringField,
     validators,
-    DateTimeField,
-    TextAreaField,
-    IntegerField,
-    EmailField
+    EmailField,
+    ValidationError
 )
 
 # todo: all fields are required -> input required
 class EmailRequest(Form):
- 	to = EmailField("to", [validators.Email])
-	to_name = StringField("to_name", []) # is string
-	from_email = EmailField("from", [validators.Email])
-	subject = StringField("subject", []) # is string
-	body = StringField("body", []) # are there any concerns with html payloads?
+    def is_string(form, field):
+        form_is_not_string = not isinstance(field, str)
+        if(form_is_not_string):
+            raise ValidationError('Body must be a string of data')
+
+    to = EmailField("to", [validators.DataRequired, validators.Email])
+    to_name = StringField("to_name", [validators.DataRequired, is_string])
+    from_email = EmailField("from", [validators.DataRequired, validators.Email])
+    subject = StringField("subject", [validators.DataRequired, is_string])
+    body = StringField("body", [validators.DataRequired, is_string])
  
-	def validate_body(form, field):
-			form_is_not_string = !(isinstance(test_string, str))
-			if(form_is_not_string):
-				raise ValidationError('Body must be a string of data')
-
-
-
-class AdminProfileForm(ProfileForm):
-    username = StringField("Username", [validators.Length(max=40)])
-    level = IntegerField("User Level", [validators.NumberRange(min=0, max=10)])
