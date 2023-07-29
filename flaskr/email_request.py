a1a7f54275a8de54e24c770d77bc79dc045fba63
Albert Snow <ajsnow2012@gmail.com>
2023-07-28 22:32:12 -0500

    swap to input

--- flaskr/email_request.py
+++ flaskr/email_request.py
@@ -13,9 +13,9 @@ class EmailRequest(Form):
         if(form_is_not_string):
             raise ValidationError('Body must be a string of data')
 
-    to = EmailField("to", [validators.DataRequired, validators.Email])
-    to_name = StringField("to_name", [validators.DataRequired, is_string])
-    from_email = EmailField("from", [validators.DataRequired, validators.Email])
-    subject = StringField("subject", [validators.DataRequired, is_string])
-    body = StringField("body", [validators.DataRequired, is_string])
+    to = EmailField("to", [validators.InputRequired, validators.Email])
+    to_name = StringField("to_name", [validators.InputRequired, is_string])
+    from_email = EmailField("from", [validators.InputRequired, validators.Email])
+    subject = StringField("subject", [validators.InputRequired, is_string])
+    body = StringField("body", [validators.InputRequired, is_string])
  
