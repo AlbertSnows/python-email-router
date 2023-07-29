e840b281d265685014133e7096ed82b7921f016f
Albert Snow <ajsnow2012@gmail.com>
2023-07-29 13:57:42 -0500

    adding pyrsistent

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,4 @@
 from flaskr.server_setup import build_app
-# from werkzeug.datastructures import ImmutableMultiDict
 
 def create_app(test_config=None):
     return build_app(test_config)
--- flaskr/routes/email.py
+++ flaskr/routes/email.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, request, jsonify
 from flaskr.utility.file import load_json_from_file
 from jsonschema import validate, exceptions, FormatChecker
+from pyrsistent import m
 
 bp = Blueprint("email", __name__, url_prefix="/email")
 
-@bp.route("/", methods=["POST"])
-def process_email_routing_request():
+def validate_input():
 	email_schema = load_json_from_file('./flaskr/schema/email.json')
 	try:
 		validate(request.json, email_schema, format_checker=FormatChecker())
 		return request.json
 	except exceptions.ValidationError as e:
-		return jsonify(error=e.message)   
-	
-      
+		return m(error=e.message)   
+  
+
+@bp.route("/", methods=["POST"])
+def process_email_routing_request():
+  validated_json = validate_input()
+  resp_payload = m(message="Email Routed") if (not "error" in validated_json) else validated_json
+  resp = jsonify(resp_payload)
+  return resp
+  
