b9a98340751690a26dda6a088c2c9bf7a3a51f28
Albert Snow <ajsnow2012@gmail.com>
2023-07-29 12:32:26 -0500

    adding email validator

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, jsonify, render_template
 from markupsafe import escape
 from werkzeug.datastructures import ImmutableMultiDict
-from jsonschema import validate, exceptions
+from jsonschema import validate, exceptions, FormatChecker
 import json
 
 def create_app(test_config=None):
@@ -40,6 +40,9 @@ def create_app(test_config=None):
     }
     
     schema = {
+      "$schema": "https://json-schema.org/draft/2020-12/schema",
+      "title": "Email Request Handler",
+      "description": "Schema to define expected json payload for email routing",
       "type": "object",
       "properties": {
         "to": {
@@ -61,36 +64,17 @@ def create_app(test_config=None):
           "type": "string"},
         "body": {
           "description": "Email Description",
-          "type": "string"}}}
+          "type": "string"}},
+              "required": ["to", "to_name", "from", "from_name", "subject", "body"]}
 
     @app.route("/email", methods=["POST"])
     def process_email_routing_request():
-      content_type = request.headers.get("Content-Type")
-      valid_json = content_type == "application/json" and request.json
-      json = request.json if valid_json else "Either incorrect content type or not a post request"
-      valid_json = validate(json) if valid_json else "Cannot validate payload"
-      data_map = ImmutableMultiDict(json)
-      
       try:
-        validate(json.loads(example_json), schema)
-        print("Validation successful. The data is valid against the schema.")
-      except exceptions.ValidationError as e:
-        print(f"Validation failed. Error: {e.message}")
-      
-      # form = email_request.EmailRequest(data_map) if valid_json else "Content-Type not supported!"
-      return jsonify()
-      populated_form = form #form.populate_obj(json)
-      valid_form = valid_json and form.validate()
-      valid_request = form.validate()
-      if not valid_json:
-        return form
-      elif not valid_request:
+        validate(request.json, schema, format_checker=FormatChecker())
         return request.json
-      elif not valid_form:
-        return jsonify(populated_form.errors)
-      else:
-        return jsonify(populated_form.data)
-
+      except exceptions.ValidationError as e:
+        return jsonify(error=e.message)      
+      
     @app.route("/hello")
     def hello():
         return "Hello, World!"
