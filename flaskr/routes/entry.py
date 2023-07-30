6f23c5058c5fe272fd07fe2525e82f712fca053a
Albert Snow <ajsnow2012@gmail.com>
2023-07-29 23:07:27 -0500

    formatting

--- flaskr/routes/entry.py
+++ flaskr/routes/entry.py
@@ -1,16 +1,22 @@
 from flask import Blueprint, jsonify
+
 bp = Blueprint("entry", __name__, url_prefix="/entry")
 from markupsafe import escape
 
+
 @bp.route("/")
 def entry():
-		return jsonify(message="Entry point! Send your data to the /email route")
+    return jsonify(message="Entry point! Send your data to the /email route")
+
+
 bp.add_url_rule("/", endpoint="index")
+
 
 @bp.route("/hello")
 def hello():
-		return "Hello, World!"
-	
+    return "Hello, World!"
+
+
 @bp.route("/write-any-text-after-slash/<text>")
 def anytext(text):
-		return f"Text: {escape(text)}"
+    return f"Text: {escape(text)}"
